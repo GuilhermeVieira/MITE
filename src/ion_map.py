71951b9be7cbf8220f4b38e83233d5aa13390dc3
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2019-05-15 23:16:13 -0300

    Added the ion intensity map constructor module

---
+++ src/ion_map.py
@@ -0,0 +1,44 @@
+##################################################################################
+##                                                                              ##
+##   Module that constructs the ion intensity map (or matrix of intensity by    ##
+##   time of elution) from a CSV file containing the m/z and the elution time   ##
+##   of each ion detected.                                                      ##
+##                                                                              ##
+##   This file is part of the featsel program                                   ##
+##   Copyright (C) 2019 Gustavo Mendes Maciel                                   ##
+##                                                                              ##
+##   This program is free software: you can redistribute it and/or modify       ##
+##   it under the terms of the GNU General Public License as published by       ##
+##   the Free Software Foundation, either version 3 of the License, or          ##
+##   (at your option) any later version.                                        ##
+##                                                                              ##
+##   This program is distributed in the hope that it will be useful,            ##
+##   but WITHOUT ANY WARRANTY; without even the implied warranty of             ##
+##   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              ##
+##   GNU General Public License for more details.                               ##
+##                                                                              ##
+##   You should have received a copy of the GNU General Public License          ##
+##   along with this program.  If not, see <http://www.gnu.org/licenses/>.      ##
+##                                                                              ##
+##################################################################################
+
+import numpy as np
+import pandas as pd
+from scipy.sparse import coo_matrix
+
+# Returns a ion intensity map given a CSV file
+def constructIonMap(infile, mz_round=2, etime_round=2):
+    df = pd.read_csv(infile,
+                     index_col='#',
+                     header=0,
+                     names=['#', 'mz', 'etime'])
+    row = np.array(df.mz.values)
+    col = np.array(df.etime.values)
+    data = np.ones(row.shape)
+    row *= 10 ** mz_round
+    col *= 10 ** etime_round
+    row = np.trunc(row)
+    col = np.trunc(col)
+    mite = coo_matrix((data, (row, col)),
+                      shape=(int(np.amax(row) + 1), int(np.amax(col) + 1)))
+    return mite
