aa616fd5c89ca89b51921997c4d327d2d790adfa
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2019-05-22 15:53:06 -0300

    Plots with title and labels

--- test/test_ion_map.py
+++ test/test_ion_map.py
@@ -6,22 +6,35 @@ import pytest
 from scipy.sparse import isspmatrix_coo, isspmatrix_csr
 
 def test_construction():
-    mite = im.constructIonMap('example_pgiq.csv')
+    file = 'example_pgiq.csv'
+    mite = im.constructIonMap(file)
     assert isspmatrix_coo(mite)
 
     plt.spy(mite, markersize=0.5, aspect='auto')
+    plt.title(file)
+    plt.xlabel('retention time')
+    plt.ylabel('m/z')
     plt.show()
 
 def test_intersection():
-    mite1 = im.constructIonMap('example_pgiq.csv')
-    mite2 = im.constructIonMap('example_pgiq.csv')
+    file1 = 'example_pgiq.csv'
+    file2 = 'example_pgiq.csv'
+    mite1 = im.constructIonMap(file1)
+    mite2 = im.constructIonMap(file2)
     intersection = im.ionMapIntersection(mite1, mite2)
     assert isspmatrix_csr(intersection)
 
     plt.subplot(131)
     plt.spy(mite1, markersize=0.5, aspect='auto')
+    plt.title(file1)
+    plt.xlabel('retention time')
+    plt.ylabel('m/z')
     plt.subplot(132)
     plt.spy(mite2, markersize=0.5, aspect='auto')
+    plt.title(file2)
+    plt.xlabel('retention time')
     plt.subplot(133)
     plt.spy(intersection, markersize=0.5, aspect='auto')
+    plt.title('Intersection')
+    plt.xlabel('retention time')
     plt.show()
