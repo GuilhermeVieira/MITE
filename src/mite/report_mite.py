b7bd4958789d60b3c57f341e535dd6fd6eddaf49
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2019-10-17 00:40:32 -0300

    Adapted the report.py file to mites (report_mite.py)

---
+++ src/mite/report_mite.py
@@ -0,0 +1,51 @@
+##################################################################################
+##                                                                              ##
+##  Module to create a report of the results obtained. By using the CADM code   ##
+##  to compare the pylogenetic trees obtained with the mtDNA tree.              ##
+##                                                                              ##
+##   This file is part of the featsel program                                   ##
+##   Copyright (C) 2018 Victor Wichmann Raposo                                  ##
+##                                                                              ##
+##   This program is free software: you can redistribute it and/or modify       ##
+##   it under the terms of the GNU General Public License as published by       ##
+##   the Free Software Foundation, either version 3 of the License, or          ##
+##   (at your option) any later version.                                        ##
+##                                                                              ##
+##   This program is distributed in the hope that it will be useful,            ##
+##   but WITHOUT ANY WARRANTY; without even the implied warranty of             ##
+##   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              ##
+##   GNU General Public License for more details.                               ##
+##                                                                              ##
+##   You should have received a copy of the GNU General Public License          ##
+##   along with this program.  If not, see <http://www.gnu.org/licenses/>.      ##
+##                                                                              ##
+##################################################################################
+
+import sys
+sys.path.append("..")
+
+from CADM import CADM
+import os 
+
+dirname = '../../output/nexus/'
+directory = os.fsencode(dirname)
+
+gen = '../../input/genomic/genomic.nex.con.tre'
+
+# Compare with others 
+
+print("Report comparing topology using the CADM test of the resulting trees"
+      "with the tree obtained with the genomic data.")
+print(30*("-"))
+
+for d in os.listdir(directory):
+    dname = os.fsdecode(d)
+
+    if (not os.path.isdir(dirname + dname)):
+        continue
+
+    print('Target: ' + dname)
+    tree = '{0}{1}/mite.nex.con.tre'.format(dirname, dname)
+    CADM(gen, tree, '-t')
+    print(30*("-"))
+
