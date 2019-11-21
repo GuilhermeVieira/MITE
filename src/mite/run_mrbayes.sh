f2c6e2595b63f5589130490a3bbede4c3c64436d
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2019-11-20 22:07:24 -0300

    Checking command line argument (number of processors)

--- src/mite/run_mrbayes.sh
+++ src/mite/run_mrbayes.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-for dir in ../../output/nexus/*; do
-    mpirun -np $1 mb "$dir/mite.nex"
-done
+if [ -z "$1" ]
+then
+    echo "You have to specify the number of processors that will be used to run MrBayes!"
+else
+    for dir in ../../output/nexus/*; do
+        echo $dir
+        mpirun -np $1 mb "$dir/mite.nex"
+    done
+fi
